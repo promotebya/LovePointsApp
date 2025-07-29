7942dd8c9477c9edccfb14605bcc18bd4cb519e4
Andrej Kunz <haus3012@web.de>
2025-07-29 15:44:25 +0200

    Working login + HomeScreen redirection

--- App.tsx
+++ App.tsx
@@ -1,19 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
 import { useAuthListener } from './hooks/useAuthListener';
 import AppNavigator from './navigation/AppNavigator';
 import AuthNavigator from './navigation/AuthNavigator';
+
 
 export default function App() {
   const { user, loading } = useAuthListener();
 
   if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
+    return <ActivityIndicator style={{ flex:1 }} size="large" />;
   }
 
   return (
@@ -22,3 +19,4 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
