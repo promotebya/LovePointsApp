580fc8d18193a2282802f23392056cf96e6e3bff
Andrej Kunz <haus3012@web.de>
2025-07-29 15:58:26 +0200

    Fix Firebase auth persistence with initializeAuth

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,18 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeApp } from "firebase/app";
+import {
+  getReactNativePersistence,
+  initializeAuth
+} from "firebase/auth/react-native";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9GMS43chgVSHXCH7i0A8FgACapq7uC38",
-  authDomain: "lovepointsapp-23880.firebaseapp.com",
-  projectId: "lovepointsapp-23880",
-  storageBucket: "lovepointsapp-23880.appspot.com",
-  messagingSenderId: "9974481581",
-  appId: "1:9974481581:web:870b7f9ab8f50cdebdfc24",
-  measurementId: "G-2PCYJEGDT5"
+  // your firebase config
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { auth };
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 
+export const db = getFirestore(app);
